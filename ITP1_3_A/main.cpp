115baefa3ae2b8655582e0864a182b81ad69c802
formula <formula22_1@yahoo.co.jp>
2016-12-25 03:15:28 +0900

    [AOJ] ITP1_3_A Print Many Hello World

---
+++ ITP1_3_A/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/**
+AOJ ITP1_3_A Print Many Hello World
+http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_A
+やるだけ.
+*/
+int main() {
+    for (int i=0;i<1000;i++) cout << "Hello World" << endl;
+    return 0;
+}
