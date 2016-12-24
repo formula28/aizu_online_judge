c484064a405b2db339c7de44b5bc234eac5ba91f
formula <formula22_1@yahoo.co.jp>
2016-12-24 23:12:11 +0900

    [AOJ] ITP1_1_B X Cubic

---
+++ ITP1_1_B/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/**
+AOJ ITP1_1_B X Cubic.
+http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_B
+ただ出力するだけ.
+*/
+int main() {
+    int d;
+    cin >> d;
+    cout << d * d * d << endl;
+    return 0;
+}
