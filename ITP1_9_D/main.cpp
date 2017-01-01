14bc0a386283a0b7ea6e258445a2a104de002738
formula <formula22_1@yahoo.co.jp>
2017-01-01 18:52:18 +0900

    [AOJ] ITP1_9_D Transformation

---
+++ ITP1_9_D/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/**
+AOJ ITP1_9_D Transformation.
+http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_9_D
+文字列変換.
+*/
+int main() {
+    string str,op,rstr;
+    int q,a,b,i;
+    cin >> str >> q;
+    for (i=0;i<q;i++) {
+        cin >> op >> a >> b;
+        if (op == "print") {
+            cout << str.substr(a, b-a+1) << endl;
+        } else if (op == "reverse") {
+            rstr = str.substr(a, b-a+1);
+            for (int j=0;j<b-a+1;j++) {
+                str[a+j] = rstr[b-a-j];
+            }
+        } else if (op == "replace") {
+            cin >> rstr;
+            str.replace(a, b-a+1, rstr);
+        }
+    }
+    return 0;
+}
