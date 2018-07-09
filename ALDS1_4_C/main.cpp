bf475a1ed3e88c33692c7ca92eed5efb3f04312c
formula <formula22_1@yahoo.co.jp>
2018-07-10 03:06:09 +0900

    [AOJ] ALDS1_4_C Dictionary

---
+++ ALDS1_4_C/main.cpp
@@ -0,0 +1,32 @@
+/**
+AOJ ALDS1_4_C: Dictionary
+http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_4_C
+
+result:
+http://judge.u-aizu.ac.jp/onlinejudge/review.jsp?rid=3011912
+Judge: 10/10 	C++ 	CPU: 00:06 sec 	Memory: 7716 KB 	Length: 359 B 	2018-07-09 23:30
+*/
+#include <iostream>
+#include <string>
+#include <set>
+using namespace std;
+ 
+int main() {
+    int n,i;
+    string op,str;
+    set<string> dict;
+    cin>>n;
+    for (i=0;i<n;i++) {
+        cin>>op>>str;
+        if (op == "insert") {
+            dict.insert(str);
+        } else if (op == "find") {
+            if (dict.count(str)>0) {
+                cout<<"yes"<<endl;
+            } else {
+                cout<<"no"<<endl;
+            }
+        }
+    }
+    return 0;
+}
